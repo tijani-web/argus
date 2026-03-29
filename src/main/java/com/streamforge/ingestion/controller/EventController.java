c51a11c90dfc5b25823eaca2803cd2472d3c1a6c
Tijani Basit <tijanibwebdev@gmail.com>
2026-03-29 16:23:24 -0700

    fix: compile error in EventController

--- src/main/java/com/streamforge/ingestion/controller/EventController.java
+++ src/main/java/com/streamforge/ingestion/controller/EventController.java
@@ -26,8 +26,8 @@ public class EventController {
     @PostMapping
     public Mono<ResponseEntity<Void>> ingestEvent(@Valid @RequestBody StreamEvent event, ServerWebExchange exchange) {
         String clientIp = getClientIp(exchange);
-        org.slf4j.LoggerFactory.getLogger(EventController.class).info("Ingesting event for project {}. Client IP: {}", projectId, clientIp);
         java.util.UUID projectId = exchange.getAttribute("projectId");
+        org.slf4j.LoggerFactory.getLogger(EventController.class).info("Ingesting event for project {}. Client IP: {}", projectId, clientIp);
 
         StreamEvent scopedEvent = new StreamEvent(
                 event.eventId(),
